fa25e99b9f751e769f422719111926d574184780
Elsayed Hamed <gadsayed964@gmail.com>
2022-04-28 18:12:27 +0200

    datastructure practce

---
+++ array.php
@@ -0,0 +1,21 @@
+<?php
+
+// one dimension array
+
+$arr=[[1,2,3,4,5],[2,4,6,8]];
+
+$a=4;
+
+//array_unshift($arr,$a); //add to beginning of array
+//array_shift($arr); //delete from the start
+//array_push($arr,$a); // add to end of array
+//array_pop($arr); // delete from the end
+
+print_r($arr);
+
+
+//foreach ($arr as $r){
+//    echo $r;
+//}
+//echo $arr[0];
+//echo $arr[4];
